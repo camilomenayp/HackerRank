ee41705845ecfd11c9bbfddc6cc4defe3d1701f2
Wilson López Romero <wilson.lopez.romero@icloud.com>
2019-12-26 20:45:52 -0300

    WIP: Fixing code

--- HackerRank_3.js
+++ HackerRank_3.js
@@ -1,16 +1,16 @@
 function hourglassSum(arr) {
   let result = [];
-  arr.forEach((x, index_1, array_x) => {
-    if (index_1 > 3) return;
-    x.forEach((y, index_2, array_y) => {
-      if (index_2 > 3) return;
+  arr.forEach((x, index_x, array_x) => {
+    if (index_x > 3) return;
+    x.forEach((y, index_y, array_y) => {
+      if (index_y > 3) return;
       let acu = 0;
-      acu += array_y[index_2] + array_y[index_2 + 1] + array_y[index_2 + 2];
-      acu += array_x[index_1 + 1][index_2 + 1];
+      acu += array_y[index_y] + array_y[index_y + 1] + array_y[index_y + 2];
+      acu += array_x[index_x + 1][index_y + 1];
       acu +=
-        array_x[index_1 + 2][index_1] +
-        array_x[index_1 + 2][index_1 + 1] +
-        array_x[index_1 + 2][index_1 + 2];
+        array_x[index_x + 2][index_y] +
+        array_x[index_x + 2][index_y + 1] +
+        array_x[index_x + 2][index_y + 2];
       result.push(acu);
     });
   });
